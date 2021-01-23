a402d9660618fa30cc08cb2d757f97dbab7bf728
pro <ndg23@bk.ru>
2021-01-23 12:41:10 +0300

    add project

--- src/main/java/ru/ndg/crudproject/controller/UserRestController.java
+++ src/main/java/ru/ndg/crudproject/controller/UserRestController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,5 +45,10 @@ public class UserRestController {
     @PostMapping(value = "/user/update", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto) {
         return new ResponseEntity<>(userRestService.updateUser(userDto), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/user/delete/{id}")
+    public void deleteUser(@PathVariable(name = "id") Long id) {
+        userRestService.deleteUser(id);
     }
 }
