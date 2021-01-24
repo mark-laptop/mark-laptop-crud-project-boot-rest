a76abd506b5244904bff39620f80808717f57727
pro <ndg23@bk.ru>
2021-01-24 13:50:16 +0300

    add project

--- src/main/java/ru/ndg/crudproject/controller/UserRestController.java
+++ src/main/java/ru/ndg/crudproject/controller/UserRestController.java
@@ -21,7 +21,7 @@ import ru.ndg.crudproject.service.user.UserRestService;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api/v1/user/")
+@RequestMapping(value = "/api/v1/users")
 public class UserRestController {
 
     private final UserRestService userRestService;
